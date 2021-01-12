0325545242491385fbd5cc6cfbf0629fbfb94acc
Azim <66790056+Azim-js@users.noreply.github.com>
2021-01-12 13:13:32 +0530

    Update detect.py

--- github/detect.py
+++ github/detect.py
@@ -2,7 +2,10 @@ import cv2
 
 face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-img=cv2.imread("news.jpg")
+# img=cv2.imread("news.jpg")  
+# new image as input 
+
+img=cv2.imread("1.jpg")
 
 gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
