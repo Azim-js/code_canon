2ab6458ba235620f7291344c6fa8d515f9dcb5ec
Azim <66790056+Azim-js@users.noreply.github.com>
2021-01-12 13:07:41 +0530

    Add files via upload

---
+++ editimage.py
@@ -0,0 +1,15 @@
+import cv2
+
+img=cv2.imread("galaxy.jpg",0)
+
+print(type(img))
+print(img)
+
+print(img.shape)
+print(img.ndim)
+
+resize=cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/2)))
+cv2.imshow("Galaxy",resize)
+cv2.imwrite("newimg.jpg",resize)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
